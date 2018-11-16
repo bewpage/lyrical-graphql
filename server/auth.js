084e0be2e8c4af62c7c8bc3b585733783f3be0c7
bewpage <b.jedlinski@bewpage.com>
2018-11-16 10:44:21 +0100

    npm update

--- server/auth.js
+++ server/auth.js
@@ -1,5 +1,5 @@
 
 
-const MONGO_URI = 'mongodb://lyrica-bew:sialababamak@ds151169.mlab.com:51169/lyricadb';
+const MONGO_URI = 'mongodb://lyrica-bew:sialababamak-2018@ds151169.mlab.com:51169/lyricadb';
 
 module.exports = MONGO_URI;
